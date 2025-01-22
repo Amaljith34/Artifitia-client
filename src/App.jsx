bae00715a3c50583766895a7aa27c072976e95b0
Amaljith-aj <jith51541@gmail.com>
2025-01-22 18:49:35 +0530

    add product error correction

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Registration from './components/auth/Registration.jsx';
 import Products from './Pages/Products.jsx';
 import BuyProduct from './components/BuyProduct.jsx';
-import AddProduct from './components/AddProduct.jsx';
 import Login from './components/auth/Login.jsx';
 import Cart from './Pages/Cart.jsx';
 import { Productsearch } from './components/ProductSearch.jsx';
@@ -17,7 +16,6 @@ export function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/product' element={<Products />} />
           <Route path='/buy-product/:id' element={<BuyProduct />} />
-          <Route path='/add-product' element={<AddProduct />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/products' element={<Productsearch/>}/>
         </Routes>
